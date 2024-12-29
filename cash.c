5785f410b5bcb4b5af144da60f7318b95a59c066
catan2001 <nikolacatic2001@gmail.com>
2024-12-29 15:28:36 +0100

    fixed tokenization, added classification

--- cash.c
+++ cash.c
@@ -5,134 +5,189 @@
 #include <sys/wait.h>
 #include <assert.h>
 
-char pcmd[100];
 #define TRUE 1
 #define FALSE 0
 #define ERROR -1
 
-void clrs(void) {
+#define MAX_SIZE 100
+enum TOKEN_TYPE
+{
+    COMMAND,
+    ARGUMENT,
+    OPERATOR,
+    CTRL_FLOW,
+    COMMENT
+};
+
+enum TOKEN_VALUE
+{
+    PWD,
+    CLEAR
+};
+
+typedef struct token_s
+{
+    enum TOKEN_TYPE type;
+    char *value;
+} token_t;
+
+char pcmd[MAX_SIZE];
+
+/*@read_cmd
+**Function: Prints prompt and reads input from stdin*/
+void read_cmd(char *pcmd, int lcmd)
+{
+    if (isatty(fileno(stdin)))
+        fprintf(stdout, "cash> ");
+    else
+        return;
+    memset(pcmd, 0, lcmd); // necessary?
+    fgets(pcmd, lcmd, stdin);
+}
+
+/*@print_term
+**Function: Prints output to prompt*/
+int print_term(char *msg)
+{
+    if (isatty(fileno(stdin)))
+    {
+        fprintf(stdout, msg);
+        return EXIT_SUCCESS;
+    }
+
+    fprintf(stderr, "Could not output to terminal, file descriptor %d", fileno(stdin));
+    return EXIT_FAILURE;
+}
+
+void clrs(void)
+{
     system("clear");
 }
 
-char **tokenizer(char *cmd) {
-    //what if empty??
-    int i = 0;
-    int word_cnt = 1;
-    for(;;) {
-        if(*(cmd + i) == 0) break;
-        if(*(cmd + i) == ' ') word_cnt++;
-        i++;
+int compare_token(char *token, char *value)
+{
+    if (strlen(token) != strlen(value))
+        return EXIT_FAILURE;
+    for (int i = 0; token[i] != 0; ++i)
+    {
+        if (token[i] != value[i])
+            return EXIT_FAILURE;
     }
+    return EXIT_SUCCESS;
+}
 
-    char **tokens = malloc(word_cnt * sizeof(char*));
-    if(tokens == NULL) {
+char **tokenizer(char *cmd)
+{
+    if (cmd[0] == 0 || cmd[0] == '\n')
+        return NULL;
+
+    int word_cnt = 0;
+    int capacity = 2;
+    char **tokens = (char **)malloc(capacity * sizeof(char *));
+    if (tokens == NULL)
+    {
         fprintf(stderr, "ERROR: tokenizer [could not allocate memory]\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    i = 0;
-    word_cnt = 0;
     char *token = cmd;
-    tokens[word_cnt] = token;
-    // tokenizes the string
-    for(;;) {
-        if(*(cmd + i) == 0) break;
-        if(*(cmd + i) == ' ' && *(cmd+i+1) != ' ') {
-            cmd[i] = '\0'; // replace whitespace with null terminator
-            word_cnt++;
-            token = (cmd + i + 1); // reference next string 
-            tokens[word_cnt] = token;
+    for (int i = 0; cmd[i] != '\0'; ++i)
+    {
+        if (cmd[i] != ' ' && cmd[i] != '\n')
+            continue;
+        cmd[i] = '\0'; // replace white space with null terminator
+
+        if (word_cnt >= capacity) // reallocate larger memory...
+        {
+            capacity *= 2;
+            char **new_tokens = realloc(tokens, capacity * sizeof(char *));
+            if (!new_tokens) // deallocate if fail
+            {
+                fprintf(stderr, "ERROR: tokenizer [realloc failed]\n");
+                for (int j = 0; j < word_cnt; ++j)
+                    free(tokens[j]);
+                free(tokens);
+                exit(EXIT_FAILURE);
+            }
+            tokens = new_tokens;
         }
-        i++;
+        tokens[word_cnt++] = strdup(token); // Copy token into the array
+
+        while (cmd[++i] == ' '); // skip spaces
+        token = &cmd[i]; // fetch next token
     }
-    tokens[word_cnt+1] = NULL; // make last NULL just in case of counting the number of tokens;
     return tokens;
 }
 
+token_t *classify_tokens(char **token)
+{
+    token_t *ctoken = (token_t *)malloc(sizeof(token_t));
+    for (int i = 0; token[i] != NULL; i++)
+    {
+        ctoken = (token_t *)realloc(ctoken, sizeof(token_t) * (i + 1) * 2);
+        if (!compare_token(token[i], "pwd"))
+        {
+            ctoken[i].type = COMMAND;
+            ctoken[i].value = "pwd";
+        } else
+        if (!compare_token(token[i], "clear") || !compare_token(token[i], "clrs"))
+        {
+            ctoken[i].type = COMMAND;
+            ctoken[i].value = "clear";  
+            clrs();
+        }
+        else
+        {
+            ctoken[i].type = ARGUMENT;
+            ctoken[i].value = token[i];
+        }
+        printf("TOKEN: ");
+        print_term(ctoken[i].value);
+        print_term("\n");
+    }
+    return ctoken;
+}
+
+// TODO: parse the tokens into a tree...
+
 // TODO: ./execution does not work
-void exec(char *cmd) {
+void exec(char *cmd)
+{
     int len;
-    if(cmd[0] == '.' && cmd[1] == '.') {
-        len = strlen(cmd+2); // for some reason it's not NULL terminated...
-        cmd[len+1] = 0;
-        if(execl(cmd+2, cmd+2, NULL) == -1) {
+    if (cmd[0] == '.' && cmd[1] == '.')
+    {
+        len = strlen(cmd + 2); // for some reason it's not NULL terminated...
+        cmd[len + 1] = 0;
+        if (execl(cmd + 2, cmd + 2, NULL) == -1)
+        {
             fprintf(stderr, "Could not execute file\n");
         }
     }
-    if(cmd[0] == 'c' && cmd[1] == 'l' && cmd[2] == 'r' && cmd[3] == 's') clrs();
 }
 
-// TODO: implement parsing similar to Bash's one
-// Lexer...
-/*
-const char * const parse_cmd(int *argc, char **argv) {
-    if(*argc > 0) {
-        char * key_word = *argv;
-        *argv += 1;
-        *argc -= 1;
-        return key_word; 
-    }
-    return NULL;
-}
-*/
+int main(void)
+{
+    clrs();
 
-
-void read_cmd(char *pcmd, int lcmd) {
-    if(isatty(fileno(stdin)))
-        fprintf(stdout, "cash> ");
-    memset(pcmd, 0, lcmd);
-    fgets(pcmd, lcmd, stdin);
-} 
-
-int print_term(char *msg) {
-    if(isatty(fileno(stdin))) {
-        fprintf(stdout, msg);
-        return 0;
-    }
-    
-    fprintf(stderr, "Could not output to terminal, file descriptor %d", fileno(stdin));
-    return -1;
-}
-
-int main(void) {
-    /*  
-    char test[] = "test best mjau";
-    char **words = tokenizer(test);
-
-    // Print each word
-    for (int i = 0; words[i] != NULL; i++) {
-        printf("%s\n", words[i]);
-    }
-
-    // Free memory
-   free(words);
-    */
-
-    while(TRUE) {
+    while (TRUE)
+    {
         read_cmd(pcmd, sizeof(pcmd));
+        char **tokens = tokenizer(pcmd);
+        token_t *ctox = classify_tokens(tokens);
 
-        char **words = tokenizer(pcmd);
-        // Print each word
-        for (int i = 0; words[i] != NULL; i++) {
-            print_term(words[i]);
-            print_term("\n");
-        }
-
-        free(words);
+        free(tokens); // add for loop to properly free  
         exec(pcmd);
         wait(NULL);
     }
-    
     return 0;
 }
 
-    // TODO: 
-    // 1) Execute in cash! ... done
-    // 2) mkdir ...
-    // 3) go thru file system ...
-    // 4) parse commands from stdin ...
-    // 5) cash> prompt sign ... done
-    // 6) while until exits ... done
-    // 7) implement clrs for clear screen ... done
-    // 8) execute file but stay in cash! ...
-    
+// TODO:
+// 1) Execute in cash! ... done
+// 2) mkdir ...
+// 3) go thru file system ...
+// 4) parse commands from stdin ...
+// 5) cash> prompt sign ... done
+// 6) while until exits ... done
+// 7) implement clrs for clear screen ... done
+// 8) execute file but stay in cash! ...
